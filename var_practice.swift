7d0b574f68a720121ced5f74f679f7fa0416e005
hejohnson0500 <18johnsh@gmail.com>
2020-10-22 13:01:24 -0500

    Create var_practice.swift
    practicing with different variables and constants

---
+++ var_practice.swift
@@ -0,0 +1,13 @@
+var firstName = "Hope" //can only hold string
+print(firstName)
+
+var stock = 100 //can only hold numeric
+print(stock)
+
+stock = 50 
+print(stock)
+
+let lastName = "Johnson" //this is a constant
+//cannot be changed
+
+print(lastName)
