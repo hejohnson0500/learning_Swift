6a39816786f12cf2c26f7c714ecab6e8bdc4946c
hejohnson0500 <18johnsh@gmail.com>
2020-10-22 13:11:25 -0500

    Create data_types.swift
    this file demonstrates how to specify the simple variable types in swift. **Not always nescessary. just using the keyword var will work as well

---
+++ data_types.swift
@@ -0,0 +1,13 @@
+//simple types
+//don't have to specify type but you can to force that type
+//string
+var name:String = "Hope" 
+//int
+var favNum:Int = 9
+//float
+var quarter:Float = .25
+//double (just a more exact float)
+var dime:Float = .10
+//bool
+var tf:Bool = true
+//if you don;t specify, it specifies at initialization
