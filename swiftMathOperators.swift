a8eeffe9fd8928ae7df63f2ace3dcf94b75d0960
hejohnson0500 <63977114+hejohnson0500@users.noreply.github.com>
2020-11-18 15:52:59 -0600

    Add files via upload

---
+++ swiftMathOperators.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+var a:Double = 2
+var b:Double = 2
+var c:Double = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a*b*c)
+c = pow(a, a) //exponetial function
+print(c)
+c = sqrt(4)
+print(c)
+c = ceil(4.2)
+print(c)
+c = floor(4.2)
+print(c)
+
+a = a + 1;
+print(a);
+a += 1
+print(a)
